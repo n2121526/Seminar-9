27647af75fa331d89323da7cb72373e4032c665f
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-09-03 11:20:57 +0300

    Add Task 66

---
+++ Task 66/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+int SumNumbers(int start, int end)
+{
+     if (start == end) return start;
+     return (start + SumNumbers(start + 1, end));
+}
+
+Console.WriteLine($"{SumNumbers(m, n)}");
+
